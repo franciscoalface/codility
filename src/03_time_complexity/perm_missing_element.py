50b9bf1f09cda88d087c3676c32962bae6830b01
Francisco Alface <falface@gmail.com>
2020-05-22 10:57:32 -0300

    style(time-complexity): used flake8 for better linting

--- src/03_time_complexity/perm_missing_element.py
+++ src/03_time_complexity/perm_missing_element.py
@@ -1,5 +1,6 @@
 """PermMissingElem
-An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)],
+which means that exactly one element is missing.
 Your goal is to find that missing element.
 
 Write a function:
@@ -19,6 +20,7 @@ Write an efficient algorithm for the following assumptions:
   - the elements of A are all distinct;
   - each element of array A is an integer within the range [1..(N + 1)].
 """
+
 
 def solution(a):
     i = 1
