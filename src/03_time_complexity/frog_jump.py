bbf3d06b2fc86ab1694728b3fb8bb96c58d74e58
Francisco Alface <falface@gmail.com>
2020-05-22 10:55:05 -0300

    style(time-complexity): used flake8 for better linting

--- src/03_time_complexity/frog_jump.py
+++ src/03_time_complexity/frog_jump.py
@@ -1,5 +1,6 @@
 """
-A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y.
+A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to
+get to a position greater than or equal to Y.
 The small frog always jumps a fixed distance, D.
 
 Count the minimal number of jumps that the small frog must perform to reach its target.
@@ -7,7 +8,8 @@ Count the minimal number of jumps that the small frog must perform to reach its 
 Write a function:
     def solution(X, Y, D)
 
-that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to
+or greater than Y.
 
 For example, given:
   X = 10
@@ -22,6 +24,8 @@ Write an efficient algorithm for the following assumptions:
     - X, Y and D are integers within the range [1..1,000,000,000];
     - X ≤ Y.
 """
+
+
 def solution(x, y, d):
     result = (y - x) / d
     if result > int(result):
